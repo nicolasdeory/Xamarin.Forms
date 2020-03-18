7775b0f4e9817941c062f775f570449512b8b2df
Gerald Versluis <gerald@verslu.is>
2020-03-18 11:17:39 +0100

    Update ViewRenderer.cs

--- Xamarin.Forms.Platform.iOS/ViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/ViewRenderer.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 
 using RectangleF = CoreGraphics.CGRect;
 using SizeF = CoreGraphics.CGSize;
+using UIKit;
 
 #if __MOBILE__
 using NativeColor = UIKit.UIColor;
@@ -75,6 +76,13 @@ namespace Xamarin.Forms.Platform.MacOS
 			base.LayoutSubviews();
 			if (Control != null)
 				Control.Frame = new RectangleF(0, 0, (nfloat)Element.Width, (nfloat)Element.Height);
+		}
+
+		public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+		{
+			base.TraitCollectionDidChange(previousTraitCollection);
+
+			Control?.SetNeedsDisplay();
 		}
 
 		public override SizeF SizeThatFits(SizeF size)
